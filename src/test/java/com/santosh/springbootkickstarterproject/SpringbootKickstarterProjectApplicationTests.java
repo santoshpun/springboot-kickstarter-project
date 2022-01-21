c15273f158489ebd6a56c0cec4676c0d96ea8bbd
Santosh <santosh@f1soft.com>
2022-01-21 18:38:40 +0545

    first commit

---
+++ src/test/java/com/santosh/springbootkickstarterproject/SpringbootKickstarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.springbootkickstarterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKickstarterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
